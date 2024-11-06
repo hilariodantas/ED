106cf1deebdfd8173355a043b45dab0e128d9a58
hilariodantas <hilariod94@gmail.com>
2024-11-06 00:16:28 -0300

    update

--- perfilador/teste.py
+++ perfilador/teste.py
@@ -4,5 +4,14 @@ if __name__ == "__main__":
 
     p = Profiler()
     
-    # Teste
+    print("Teste para 10 valores\n")
+    p.test(selectionSort, size=10, comp=True, exch=True, trace=False)
+    
+    print("Teste para 100 valores\n")
     p.test(selectionSort, size=100, comp=True, exch=True, trace=False)
+
+    print("Teste para 1000 valores\n")
+    p.test(selectionSort, size=1000, comp=True, exch=True, trace=False)
+
+    print("Teste para 10000 valores\n")
+    p.test(selectionSort, size=10000, comp=True, exch=True, trace=False)
